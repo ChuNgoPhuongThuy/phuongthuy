b7eb4d950b83207371f24e827c51f20bc295dfa0
Chu Ngô Phương Thuỷ <91727233+ChuNgoPhuongThuy@users.noreply.github.com>
2023-04-24 22:42:52 +0700

    Update en.ts

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -23,27 +23,13 @@ const en = {
       {
         title: "Mail",
         icon: "SiGmail",
-        link: "mailto:duyntp2000@gmail.com",
+        link: "chungophuongthuy@gmail.com",
       },
-      {
-        title: "Linkedin",
-        icon: "SiLinkedin",
-        link: "https://www.linkedin.com/in/duyntp2000/",
-      },
+    
       {
         title: "Github",
         icon: "SiGithub",
-        link: "https://github.com/howznguyen",
-      },
-      {
-        title: "Gitlab",
-        icon: "SiGitlab",
-        link: "https://gitlab.com/howznguyen",
-      },
-      {
-        title: "Facebook",
-        icon: "SiFacebook",
-        link: "https://www.facebook.com/howznguyen",
+        link: "https://github.com/ChuNgoPhuongThuy",
       },
       
     ],
@@ -53,48 +39,25 @@ const en = {
   },
   home: {
     intro: {
-      header: "Hi, I'm Howz Nguyen",
+      header: "Hi, I'm Chu Ngo Phuong Thuy",
       description:
-        "I am currently a Fullstack Developer. This is a website I created with the desire to share my knowledge to everyone. Thank you to everyone who visited. ❤️",
+        "I am a Business Analyst. This is a website I created with the desire to share my experience to everyone. Thank you to everyone who visited. ❤️",
       image: "/assets/images/orion-nebula.jpg",
-      links: [
-        {
-          icon: "HiOutlineNewspaper",
-          title: "Resume",
-          link: "https://github.com/howznguyen/my-resume/blob/main/pdf/cv_en_vi_howznguyen.pdf",
-        },
-        {
-          icon: "FaFacebook",
-          title: "howznguyen",
-          link: "https://facebook.com/howznguyen/",
-        },
-        {
-          icon: "FaGithub",
-          title: "howznguyen",
-          link: "https://github.com/howznguyen",
-        },
-      ],
+      
     },
     featured_posts: "Featured Posts",
     read_more: "Read more",
     categories: [
       {
         name: "Knowledge",
-        description:
-          "Related posts about programming knowledge and algorithms.",
-        value: ["code", "algorithm"],
+        
       },
       {
         name: "Tutorial & Tips",
         description: "Tutorials or tips about programming and technology.",
         value: ["tutorial", "tip"],
       },
-      {
-        name: "Notion",
-        description:
-          "Articles with content about Notion and utilities around Notion.",
-        value: "notion",
-      },
+     
       {
         name: "Other",
         description: "The posts are not related to technology topics.",
